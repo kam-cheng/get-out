6cb0bbe009f5f3b5c881a239601effe26d6ac95b
Kam Cheng <kamcheng@hotmail.co.uk>
2022-03-23 10:38:20 +0000

    creates UserContext and default user, now shows up on Profile Page

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -1,10 +1,25 @@
-import * as React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, Button, Image, StyleSheet } from "react-native";
+import UserContext from "../context/User";
 
 export default function ProfileScreen({ navigation }) {
+  const { user, setUser } = useContext(UserContext);
+  console.log(user);
+  // React Context for User {username, avatar, rating, verified}
+
+  // Display list of activities organised matching the user
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Profile Screen</Text>
+      <Text>Welcome Back!</Text>
+      <Image
+        style={styles.logo}
+        source={{
+          uri: user.avatar,
+        }}
+      />
+      <Text>{user.name}</Text>
+      <Text>Rating: {user.rating}</Text>
       <Button
         title="Create New Activity"
         onPress={() => navigation.navigate("Create New Activity")}
@@ -12,3 +27,10 @@ export default function ProfileScreen({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 100,
+    height: 100,
+  },
+});
