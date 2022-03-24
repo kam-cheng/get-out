4d116770fe9fd7aafa05e06facedd6f1b8469178
Pri-21 <priya_now@hotmail.co.uk>
2022-03-24 12:19:09 +0000

    Renders unstyled list of activities

--- screens/Category.jsx
+++ screens/Category.jsx
@@ -1,13 +1,18 @@
 import * as React from "react";
 import { View, Text, Button } from "react-native";
+import { ui, text } from "../theme";
+import ActivityCard from "../components/cards/ActivityCard";
+import ActivityList from "../containers/ActivityList";
 
 export default function CategoryScreen({ route, navigation }) {
   const { name } = route.params;
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Name: {JSON.stringify(name)}</Text>
-
-      <Button title="Home" onPress={() => navigation.navigate("Home")} />
+    <View style={ui.container}>
+      <View>
+      <Text style={text.body}>Browsing Activities</Text>
+      <Text style={text.subtitle}>{JSON.stringify(name)}</Text>
+      </View>
+      <ActivityList/>
     </View>
   );
 }
