38dd87d5c2142301727bb2763ef59039bf846282
Kam Cheng <kamcheng@hotmail.co.uk>
2022-03-24 15:58:22 +0000

    timestamps now display without errors

--- api/fetchDocuments.js
+++ api/fetchDocuments.js
@@ -1,15 +1,25 @@
 import firestore from "@react-native-firebase/firestore";
 
 const fetchDocuments = async ({ collection, key, query, setState, value }) => {
+  const time = firestore.Timestamp.now();
+  console.log("Current Time ", time.toDate().toString());
   const arrayofactivities = [];
-  await firestore()
+  const ref = await firestore()
     .collection(collection)
     .where(key, query, value)
-    .onSnapshot((querySnapshot) => {
+    .where("Date", ">=", time);
+  console.log(ref.length);
+
+  ref.onSnapshot(
+    (querySnapshot) => {
       querySnapshot.docChanges().forEach((change) => {
         if (change.type === "added") {
           const newItem = change.doc.data();
           newItem.id = change.doc.id;
+          // console.log(firestore.Timestamp.now().toDate());
+          // convert timestamp to date
+          newItem.Date = newItem.Date.toDate().toString();
+          console.log(newItem.Date);
           arrayofactivities.push(newItem);
         }
         if (change.type === "removed") {
@@ -21,7 +31,11 @@ const fetchDocuments = async ({ collection, key, query, setState, value }) => {
       });
       // update usestate because returning does not seem to work!!
       setState(() => arrayofactivities);
-    });
+    },
+    (err) => {
+      console.log(`Encountered error: ${err}`);
+    }
+  );
 };
 
 export default fetchDocuments;
--- hooks/useActivitiesList.js
+++ hooks/useActivitiesList.js
@@ -4,7 +4,6 @@ import fetchDocuments from "../api/fetchDocuments";
 const useActivitiesList = (props) => {
   const [activities, setActivities] = useState(null);
   props.setState = setActivities;
-  console.log(props);
 
   useEffect(() => {
     async function getDocuments() {
