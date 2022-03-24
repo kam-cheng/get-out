5b101ff7faf5315abbc772e7223669a81cca1bcc
Kam Cheng <kamcheng@hotmail.co.uk>
2022-03-24 21:56:53 +0000

    fixes bug where activities were not updating immediately

--- containers/ActivitiesList.jsx
+++ containers/ActivitiesList.jsx
@@ -1,33 +1,34 @@
-import React, { useContext } from "react";
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
 import { Text, View } from "react-native";
-import useActivitiesList from "../hooks/useActivitiesList";
-import UserContext from "../context/User";
+// import useActivitiesList from "../hooks/useActivitiesList";
 import { text } from "../theme";
+import fetchDocuments from "../api/fetchDocuments";
 
 export default function ActivitiesList({ title, props }) {
-  const {
-    user: { name },
-  } = useContext(UserContext);
+  const [activities, setActivities] = useState([]);
 
-  // const organised = useOrganisedActivities(name);
-  const organised = useActivitiesList(props);
-  if (organised !== null && organised.length > 0) {
-    return (
-      <>
-        <Text style={text.subtitle}>{title}</Text>
-        {organised.map((activity) => (
-          <View
-            key={activity.id}
-            style={{ backgroundColor: "pink", margin: 10, padding: 10 }}
-          >
-            <Text style={text.body}>{activity.Activity}</Text>
-            <Text style={text.body}>{activity.Image}</Text>
-            <Text style={text.body}>{activity.Date}</Text>
-          </View>
-        ))}
-      </>
-    );
+  // eslint-disable-next-line react/prop-types
+  props.setState = setActivities;
+
+  fetchDocuments(props);
+
+  if (activities.length === 0) {
+    return <Text style={text.subtitle}>Loading Activities...</Text>; // or a spinner
   }
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <></>;
+  return (
+    <>
+      <Text style={text.subtitle}>{title}</Text>
+      {activities.map((activity) => (
+        <View
+          key={activity.id}
+          style={{ backgroundColor: "pink", margin: 10, padding: 10 }}
+        >
+          <Text style={text.body}>{activity.Activity}</Text>
+          <Text style={text.body}>{activity.Image}</Text>
+          <Text style={text.body}>{activity.Date}</Text>
+        </View>
+      ))}
+    </>
+  );
 }
