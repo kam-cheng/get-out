0d9f650328dce66e340c4acd9277e92d14fec011
Samuel Parfait <samuelparfaitjr@gmail.com>
2022-03-25 12:59:24 +0000

    added ScrollView to Category.jsx

--- screens/Category.jsx
+++ screens/Category.jsx
@@ -1,17 +1,19 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { ui, text } from "../theme";
 import ActivityList from "../containers/ActivityList";
 
 export default function CategoryScreen({ route, navigation }) {
   const { name } = route.params;
   return (
-    <View style={ui.container}>
-      <View>
-        <Text style={text.body}>Browsing Activities</Text>
-        <Text style={text.sectionTitle}>{JSON.stringify(name)}</Text>
+    <ScrollView>
+      <View style={ui.container}>
+        <View>
+          <Text style={text.body}>Browsing Activities</Text>
+          <Text style={text.sectionTitle}>{JSON.stringify(name)}</Text>
+        </View>
+        <ActivityList navigation={navigation} />
       </View>
-      <ActivityList navigation={navigation} />
-    </View>
+    </ScrollView>
   );
 }
