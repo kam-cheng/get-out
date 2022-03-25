b97e6e0662027d9126ebb93a504ca965f5914037
Tom Harris <tomharris3@me.com>
2022-03-25 13:00:52 +0000

    access to props in activity list

--- containers/ActivityList.jsx
+++ containers/ActivityList.jsx
@@ -2,18 +2,16 @@ import React, { useState, useEffect } from "react";
 
 import fetchDocuments from "../api/fetchDocuments";
 
-
 import ActivityCard from "../components/cards/ActivityCard";
 
 export default function ActivityList({ navigation, props }) {
   const [activityList, setActivityList] = useState([]);
-  console.log(props)
 
-useEffect(()=> {
-  fetchDocuments("activities")
-})
+  const { key, query, value } = props;
 
-
+  useEffect(() => {
+    fetchDocuments("activities");
+  });
 
   const handlePress = (title) => {
     navigation.navigate("Activity", { title });
@@ -22,7 +20,6 @@ useEffect(()=> {
   return outdoors.map((item) => (
     <ActivityCard
       key={item.id}
-     
       title={item.title}
       img_url={item.img_url}
       handlePress={handlePress}
