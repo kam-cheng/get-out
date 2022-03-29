7727006b510a56a15a06231c7489d7b080c01e30
Kam Cheng <kamcheng@hotmail.co.uk>
2022-03-29 11:16:07 +0100

    fixed bug where loading activities still shows when the array is empty

--- containers/ActivitiesList.jsx
+++ containers/ActivitiesList.jsx
@@ -8,18 +8,19 @@ import ActivityCard from "../components/cards/ActivityCard";
 
 export default function ActivitiesList({ heading, props }) {
   const [activities, setActivities] = useState([]);
+  const [loading, setLoading] = useState(false);
   const navigation = useNavigation();
 
   props.setState = setActivities;
 
-  fetchDocuments(props);
+  fetchDocuments(props, setLoading);
 
   const handlePress = (item) => {
     navigation.navigate("Activity", { item });
   };
 
-  if (activities.length === 0)
-    return <Text style={text.subtitle}>Loading Activities...</Text>; // or a spinner
+  if (loading) return <Text style={text.subtitle}>Loading Activities...</Text>; // or a spinner
+  if (activities.length === 0) return <Text>?</Text>;
   return (
     <>
       <Text style={text.profileSectionTitle}>{heading}</Text>
