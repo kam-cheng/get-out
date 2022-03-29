3077060c798fc7a7accc9e51ac4c976c7b8234f7
Kam Cheng <kamcheng@hotmail.co.uk>
2022-03-29 17:08:42 +0100

    implemented reviews display and basic styling

--- containers/ReviewsList.jsx
+++ containers/ReviewsList.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { ui, text } from "../theme";
 
 export default function ReviewsList({ reviews }) {
+  if (!reviews) return <></>;
   // separate out all ratings
   const ratingsArray = reviews.map((review) => review.rating);
   const average = (list) =>
     list.reduce((prev, curr) => prev + curr) / list.length;
-  const ratingsAverage = average(ratingsArray);
+  const ratingsAverage = average(ratingsArray).toFixed(1);
   return (
-    <View>
-      <Text>Reviews List</Text>
-      <Text>Average Rating: {ratingsAverage}</Text>
+    <View style={ui.container}>
+      <Text style={text.sectionTitleAlt}>Reviews List</Text>
+      <Text style={text.sectionTitleAlt}>Average Rating: {ratingsAverage}</Text>
       {reviews.map((review) => (
         <View key={review.user}>
-          <Text>User: {review.user}</Text>
-          <Text>Rating: {review.rating}</Text>
-          <Text>Comment: {review.review}</Text>
+          <Text style={text.body}>User: {review.user}</Text>
+          <Text style={text.body}>Rating: {review.rating}</Text>
+          <Text style={text.body}>Comment: {review.review}</Text>
         </View>
       ))}
     </View>
