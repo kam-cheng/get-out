74428f6a0cacb41f662e3d25af1a5350a9943e1c
Tom Harris <tomharris3@me.com>
2022-03-30 15:29:01 +0100

    added dropdown, issues with bits

--- containers/UsersList.jsx
+++ containers/UsersList.jsx
@@ -1,21 +1,37 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import fetchCollection from "../api/fetchCollection";
 import UsersCard from "../components/cards/UsersCard";
+import UserContext from "../context/User";
+import DropDownPicker from "react-native-dropdown-picker";
+import { Text } from "react-native";
 
-export default function UsersList () {
-    const [usersList, setUsersList] = useState([])
+export default function UsersList() {
+  const [usersList, setUsersList] = useState([]);
 
-    useEffect(() => {
-        fetchCollection("users").then((users) => {
-            setUsersList(users)
-        })
-    }, [])
+  useEffect(() => {
+    fetchCollection("users").then((data) => setUsersList(data));
+  }, []);
 
-    return usersList.map((item) => {
-        <UsersCard
-        key={item.name}
-        name={item.name}
-        avartarUrl={item.avartarUrl}
-        />
-    })
+  // const userItems = usersList.map((user) => {
+  //   const obj = {
+  //     label: user.name,
+  //     value: user.id,
+  //   };
+  //   return obj;
+  // });
+
+  // const [open, setOpen] = useState(false);
+  // const [value, setValue] = useState(null);
+  // const [items, setItems] = useState(userItems);
+
+  return (
+    <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+    />
+  );
 }
